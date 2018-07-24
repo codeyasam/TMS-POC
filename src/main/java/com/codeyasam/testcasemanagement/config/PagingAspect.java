773de358abb370746f50faa35c39df169c94ed08
codeyasam <codeyasam@gmail.com>
2018-07-24 22:36:34 +0800

    added an executable class and paging config

---
+++ src/main/java/com/codeyasam/testcasemanagement/config/PagingAspect.java
@@ -0,0 +1,28 @@
+package com.codeyasam.testcasemanagement.config;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+@Aspect
+public class PagingAspect {
+	
+	@Pointcut("within(com.citco.qa.system.restcontroller.*) && (args(pageable,..) || args(..,pageable))")
+	public void restControllerGetMethods(Pageable pageable) {}
+	
+	@Around("restControllerGetMethods(pageable)")
+	public Object Pageable(ProceedingJoinPoint pjp, Pageable pageable) throws Throwable {
+		int pageNumber = pageable.getPageNumber() > 0 ? pageable.getPageNumber() - 1 : 0;
+		Object[] args = pjp.getArgs();
+		for (int i = 0; i < args.length; i++) {
+			if (args[i] instanceof Pageable) {
+				pageable = PageRequest.of(pageNumber, pageable.getPageSize(), pageable.getSort());
+				args[i] = pageable;
+			}
+		}
+		return pjp.proceed(args);
+	}
+}
