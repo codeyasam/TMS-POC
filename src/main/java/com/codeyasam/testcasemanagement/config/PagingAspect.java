dd44a1d4bfbea7aa14142e191ca48d1c6458429b
codeyasam <codeyasam@gmail.com>
2018-07-27 00:07:28 +0800

    configured aop paging aspect for 1-based indexed

--- src/main/java/com/codeyasam/testcasemanagement/config/PagingAspect.java
+++ src/main/java/com/codeyasam/testcasemanagement/config/PagingAspect.java
@@ -2,12 +2,12 @@ package com.codeyasam.testcasemanagement.config;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-@Aspect
+//@Aspect
+//@Component
 public class PagingAspect {
 	
 	@Pointcut("within(com.citco.qa.system.restcontroller.*) && (args(pageable,..) || args(..,pageable))")
@@ -19,7 +19,7 @@ public class PagingAspect {
 		Object[] args = pjp.getArgs();
 		for (int i = 0; i < args.length; i++) {
 			if (args[i] instanceof Pageable) {
-				pageable = PageRequest.of(pageNumber, pageable.getPageSize(), pageable.getSort());
+				pageable = new PageRequest(pageNumber, pageable.getPageSize(), pageable.getSort());
 				args[i] = pageable;
 			}
 		}
